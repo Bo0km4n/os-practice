4a90db343b7e11536e1c73fbb7ea083d554fd65f
Bo0km4n <pokopenkatuya@gmail.com>
2018-05-05 15:27:48 +0900

    add day5

---
+++ day5/harib02h/include/prot.h
@@ -0,0 +1,11 @@
+#ifndef __PROT_H__
+#define __PROT_H__
+
+// lib.c
+// 標準ライブラリ的関数群
+int sprintf(char *str, const char *format, ...);
+void strcls(char *);
+//void int2hex(char *, int);
+//void int2dec(char *, int);
+
+#endif
